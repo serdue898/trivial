7bf1ea076ac7172febbfaa0782ac15455f2e099f
Carlos Montequi <132488662+CharlieMontequi@users.noreply.github.com>
2024-01-11 11:08:26 +0100

    intento minijuego test da un error al asignar el string al text view y no se porque

--- app/src/main/java/com/example/trivialnavidad/core/feature/minijuegos/adivina/view/Adivina.kt
+++ app/src/main/java/com/example/trivialnavidad/core/feature/minijuegos/adivina/view/Adivina.kt
@@ -1,18 +1,22 @@
 package com.example.trivialnavidad.core.feature.juego.view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.GridLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import com.example.t8_ej03_persistenciaapi.model.Pregunta
 import com.example.trivialnavidad.R
@@ -25,35 +29,119 @@ class Adivina(var pregunta :Pregunta,var jugador :JugadorEnPartida) : Fragment()
     private var contexto: Context? = null
 
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.minijuego_adivina, container, false)
         contexto = container?.context
 
 
-        val bt_clasificacion = view.findViewById<Button>(R.id.comprobar)
-        val tx_pregunta = view.findViewById<TextView>(R.id.pregunta)
-        val texto = view.findViewById<EditText>(R.id.respuesta)
+
+        val tx_pregunta = view.findViewById<TextView>(R.id.t_pregunta)
+        val palabraAdivinar = pregunta.correcta
+        val palabraDividida = view.findViewById<EditText>(R.id.t_palabraAdivina)
         var ganado = false
         tx_pregunta.text = pregunta.pregunta
-        bt_clasificacion.setOnClickListener {
-            val respuesta = texto.text.toString()
-            var acercaDe: Int
-            if (respuesta == pregunta.correcta) {
-                 acercaDe = R.string.acierto
-                jugador.juegos[0] = true
-                ganado = true
-            } else {
-                 acercaDe = R.string.fallo
+        val gr_abecedario = view.findViewById<GridLayout>(R.id.gr_botonesLetras)
+
+        val abecedario = CharArray(27) { ('a' + it).toChar() }
+        abecedario[26] = 'ñ'
+        var palabraHuecos = desgranarPalabra(palabraAdivinar)
+        // no entiendo porque no me esta dejando asignar el string al txtview de la palabra hecha trocitos
+        palabraDividida.text= palabraHuecos
+
+        for (letra in abecedario){
+            var botonLetra: Button = Button(contexto)
+            botonLetra.text= letra.toString()
+            /* añadir los margenes a los botones para que esten separados pero me pide hacer un import que le pincho y no lo hace :S
+            botonLetra.marginBottom(5)
+            botonLetra.marginTop(5)
+            botonLetra.marginLeft(5)
+            botonLetra.marginRight(5)
+            */
+            val params = GridLayout.LayoutParams()
+            params.width = ViewGroup.LayoutParams.WRAP_CONTENT
+            params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+
+            // Agrega el botón al GridLayout con los parámetros de diseño
+            gr_abecedario.addView(botonLetra, params)
+
+            botonLetra.setOnClickListener(){
+
+                if(comprobarLetra(botonLetra, palabraAdivinar )){
+                    botonLetra.setBackgroundColor(R.color.verde)
+                    palabraHuecos=rellenarPalabra(palabraHuecos, palabraAdivinar, botonLetra.text.toString())
+                }else {
+                    botonLetra.setBackgroundColor(R.color.rojo)
+                }
+                if(comprobarPlabra(palabraHuecos)){
+                    val mensaje = AlertDialog.Builder(contexto as AppCompatActivity)
+                    mensaje.setMessage("Enhorabuena, has resuelto correctamente la prueba.")
+                    mensaje.show()
+                }else{
+
+                }
             }
-            val msnEmergente = AlertDialog.Builder(contexto as AppCompatActivity)
-            msnEmergente.setCancelable(false)
-            msnEmergente.setMessage(getString(acercaDe))
-            msnEmergente.setPositiveButton("Aceptar") { dialog, which ->
-                comunicador?.volver(contexto as AppCompatActivity , ganado)
+        }
+
+        return view
+    }
+
+    fun desgranarPalabra(palabra: String) : String{
+       var palabraDividida : String= ""
+        val arrayDeLetras = palabra.toCharArray().map { it.toString() }.toTypedArray()
+        for(hueco in arrayDeLetras){
+           palabraDividida= palabraDividida +"_ "
+        }
+        return palabraDividida
+    }
+
+    fun comprobarLetra(botonPulsado :Button, palabra: String): Boolean {
+        var correcto = false
+        val arrayDeLetras = palabra.toCharArray().map { it.toString() }.toTypedArray()
+        for(hueco in arrayDeLetras){
+            if(botonPulsado.text== hueco){
+                correcto= true
             }
-            msnEmergente.show()
+        }
+
+        return correcto
+    }
+    fun comprobarPlabra(palabraDividida:String): Boolean{
+        var completa= false
+        val arrayDeLetras = palabraDividida.toCharArray().map { it.toString() }.toTypedArray()
+        for (hueco in palabraDividida){
+            if (hueco.equals("_")){
+                return completa
+            }else{
+                completa= true
+            }
+        }
+
+        return completa
+    }
+    fun rellenarPalabra(palabraDividida: String, palabraAdivinar: String, letra: String) : String{
+        var palabraHuecosActualizados = ""
+        val letrasAdivinar = palabraAdivinar.toCharArray().map { it.toString() }.toTypedArray()
+        val letrasDividida = palabraDividida.toCharArray().map { it.toString() }.toTypedArray()
+        var posicion = 0
+
+        for(letraAdivinar in letrasAdivinar){
+            var i =0
+            if(letraAdivinar == letra){
+                posicion= i
+            }
+            i++
+        }
+        for(letrahueco in letrasDividida){
+            var i = 0
+            if(i == posicion){
+                palabraHuecosActualizados= palabraHuecosActualizados + letra
+            }else{
+                palabraHuecosActualizados= palabraHuecosActualizados + "_ "
+            }
 
         }
-        return view
+
+        return  palabraHuecosActualizados
     }
 }
