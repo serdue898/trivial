23b9774446557fda308b5b25a383b41727bd292e
serdue898 <serduer898@gmail.com>
2024-01-25 10:52:29 +0100

    ahorcado terminado

--- app/src/main/java/com/example/trivialnavidad/core/feature/minijuegos/adivina/view/Adivina.kt
+++ app/src/main/java/com/example/trivialnavidad/core/feature/minijuegos/adivina/view/Adivina.kt
@@ -22,6 +22,7 @@ import java.text.Normalizer
 class Adivina(var pregunta :Pregunta,var jugador :JugadorEnPartida) : Fragment() {
     private var comunicador: ComunicadorAdivina? = MetodosAdivina()
     private var contexto: Context? = null
+    private var fallos = 0
 
 
 
@@ -29,9 +30,11 @@ class Adivina(var pregunta :Pregunta,var jugador :JugadorEnPartida) : Fragment()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.minijuego_adivina, container, false)
         contexto = container?.context
+        var imagenes = arrayOf(R.drawable.ahorcado_1, R.drawable.ahorcado_2, R.drawable.ahorcado_3, R.drawable.ahorcado_4, R.drawable.ahorcado_5, R.drawable.ahorcado_6)
 
 
 
+        val imagen = view.findViewById<androidx.appcompat.widget.AppCompatImageView>(R.id.imagenAhorcado)
         val tx_pregunta = view.findViewById<TextView>(R.id.t_pregunta)
         val palabraAdivinar = Normalizer.normalize(pregunta.correcta[0], Normalizer.Form.NFD)
             .replace("[^\\p{ASCII}]".toRegex(), "")
@@ -69,6 +72,11 @@ class Adivina(var pregunta :Pregunta,var jugador :JugadorEnPartida) : Fragment()
                     palabraDividida.text= palabraHuecos
                 }else {
                     botonLetra.background = ContextCompat.getDrawable(contexto!!, R.drawable.fondo_letra_fallada)
+                    fallos++
+                    imagen.setImageResource(imagenes[fallos-1])
+                }
+                if (fallos==6){
+                    terminarJuego(false)
                 }
                 if(comprobarPlabra(palabraHuecos.replace(" ",""))){
                     terminarJuego(true)
